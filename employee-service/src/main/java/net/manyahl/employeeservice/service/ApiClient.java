f82b5868087ec4d18f26a67b747db0ee928f3a00
Yitbarek Z. Khelbore <ytbareck@gmail.com>
2025-01-18 14:39:12 +0400

    Service registry and api gateway

--- employee-service/src/main/java/net/manyahl/employeeservice/service/ApiClient.java
+++ employee-service/src/main/java/net/manyahl/employeeservice/service/ApiClient.java
@@ -2,7 +2,6 @@ package net.manyahl.employeeservice.service;
 
 import net.manyahl.employeeservice.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
